4f653e6b9513383129bbcac96dd7c62c55a45226
Carlos Villalobos <carvilf@gmail.com>
2022-10-18 22:31:59 -0500

    creacion tablas

---
+++ database/migrations/2022_10_19_031457_crear_tabla_libro.php
@@ -0,0 +1,38 @@
+<?php
+
+use Faker\Calculator\Isbn;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libro', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('titulo',100);
+            $table->string('Isbn',30);
+            $table->string('autor',100);
+            $table->unsignedTinyInteger('cantidad');
+            $table->string('editorial',50)->nullable();
+            $table->string('foto',100)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('libro');
+    }
+};
