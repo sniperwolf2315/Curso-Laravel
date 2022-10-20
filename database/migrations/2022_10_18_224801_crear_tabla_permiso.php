d64066dbfcdc8ed2c55b3d210bcb2799651ead02
Carlos Villalobos <carvilf@gmail.com>
2022-10-19 19:23:10 -0500

    creacion de tabla observaciones

--- database/migrations/2022_10_18_224801_crear_tabla_permiso.php
+++ database/migrations/2022_10_18_224801_crear_tabla_permiso.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('nombre',50);
             $table->string('slug',50);
             $table->timestamps();
+            $table->string('observaciones',100);
         });
     }
 
